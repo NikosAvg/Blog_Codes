f92dc57a8dd23924758a5c65c2c955da07cad9e7
Nikos Avgoustis <40135168+NikosAvg@users.noreply.github.com>
2022-03-02 11:23:25 +0200

    Update montecarintegration.py

--- montecarintegration.py
+++ montecarintegration.py
@@ -1,27 +1,33 @@
 # Python code for Monte Carlo Integration.
 # Created By Nikos Avgoustis.
 
+# Import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 plt.style.use("classic")
 
+# Define our functions.
 def func(x):
     return np.sin(x)
 
 def func2d(x,y):
     return np.cos(x)*np.exp(y)
-  
+ 
+# Initialize first function parameters and compute them.
 a=0
 b=np.pi
 x = np.linspace(a,b,sampling_num)
 y = func(x)
+
+# Plot the first function.
 plt.title("Sin(x)")
 plt.xlabel("x")
 plt.ylabel("Sin(x)")
 plt.plot(x,y)
 plt.show()
 
+# Initialize second function parameters and compute them.
 x_min = 0
 x_max = np.pi/2
 y_min = 0 
@@ -31,6 +37,7 @@ y = np.linspace(y_min,y_max,sampling_num)
 X, Y = np.meshgrid(x, y)
 func = func2d(X,Y)
 
+# Plot the second function.
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.plot_surface(X, Y, func)
@@ -40,6 +47,7 @@ ax.set_zlabel('Cos(x)*Exp(y)')
 ax.view_init(40,40) 
 plt.show()
 
+# Perform Monte Carlo integration (1D Case)
 actual_val = 2
 err = []
 results = []
@@ -51,12 +59,14 @@ for i in range(num_sim):
 print("Value Calculated = {} ".format(np.mean(results)))
 print("Mean error = {}".format(np.mean(err)))
 
+# Visualize the results.
 plt.hist(results)
 plt.title("Distribution of Areas Calculated")
 plt.xlabel("Areas")
 plt.ylabel("Number of Times Calculated")
 plt.show()
 
+# Perform Monte Carlo integration (2D case)
 actual_value2 = 1.71828
 results2 = []
 err2 = []
@@ -68,6 +78,7 @@ for i in range(num_sim):
 print("Value Calculated = {} ".format(np.mean(results2)))
 print("Mean error = {}".format(np.mean(err2)))
 
+# Visualize the results.
 plt.hist(results2)
 plt.title("Distribution of Areas Calculated")
 plt.xlabel("Areas")
